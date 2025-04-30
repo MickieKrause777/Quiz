d27b5adcfaae9e015f45c51402d793aa0f68cc71
AndreasRhein <andreas.rhein@iu-study.org>
2025-04-30 20:14:59 +0200

    added dialog for game mode and footer

---
+++ components/TestComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const TestComponent: React.FC = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center", marginTop: "20px" }}>Hallo Welt!</h1>
+            <p style={{ textAlign: "center" }}>Dieser Text zeigt, dass alles funktioniert.</p>
+        </div>
+    );
+};
+
+export default TestComponent;
